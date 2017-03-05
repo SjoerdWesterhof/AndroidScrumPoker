4b2902f6238c9c0904a16a4d845d2d41cce2d57d
Vincent Keizer <v.keizer@gmail.com>
2017-03-05 11:06:57 +0100

    Added poker cards and added RecyclerView to display cards horizontally

--- app/src/main/java/com/billythebastard/scrumpoker/PlayPoker.java
+++ app/src/main/java/com/billythebastard/scrumpoker/PlayPoker.java
@@ -4,16 +4,30 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class PlayPoker extends AppCompatActivity {
 
-    private String[] _items = { "0", "1", "2", "3", "5", "8", "13", "20", "40" };
+    private PokerCard[] mPokerCards =
+            {
+                    new PokerCard("0", R.drawable.pokercard_0),
+                    new PokerCard("0.5", R.drawable.pokercard_half),
+                    new PokerCard("1", R.drawable.pokercard_1),
+                    new PokerCard("2", R.drawable.pokercard_2),
+                    new PokerCard("3", R.drawable.pokercard_3),
+                    new PokerCard("5", R.drawable.pokercard_5),
+                    new PokerCard("8", R.drawable.pokercard_8),
+                    new PokerCard("13", R.drawable.pokercard_13),
+                    new PokerCard("20", R.drawable.pokercard_20),
+                    new PokerCard("40", R.drawable.pokercard_40),
+                    new PokerCard("100", R.drawable.pokercard_100),
+                    new PokerCard("?", R.drawable.pokercard_questionmark),
+                    new PokerCard("coffee", R.drawable.pokercard_coffee)
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +50,33 @@ public class PlayPoker extends AppCompatActivity {
     }
 
     private void initializePokerList() {
-        ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, _items);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.pokercards_recyclerView);
+        recyclerView.setHasFixedSize(true);
 
-        ListView listView = (ListView) findViewById(R.id.possibleScoresListView);
-        listView.setAdapter(itemsAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String item = _items[position];
-                TextView score = (TextView) findViewById(R.id.textViewScore);
-                score.setText(item);
-            }
-        });
+        PokerCardRecyclerAdapter adapter = new PokerCardRecyclerAdapter(mPokerCards);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(this, recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        PokerCard pokerCard = mPokerCards[position];
+                        UpdateScore(pokerCard);
+                    }
+
+                    @Override public void onLongItemClick(View view, int position) {
+                        PokerCard pokerCard = mPokerCards[position];
+                        UpdateScore(pokerCard);
+                    }
+                })
+        );
+    }
+
+    private void UpdateScore(PokerCard pokerCard) {
+        ImageView score = (ImageView) findViewById(R.id.imageViewScore);
+        score.setImageResource(pokerCard.GetImageResourceId());
     }
 
 }
